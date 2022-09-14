021b81712576386f0503357fbc0fa7438ba54965
ItsRosemary79 <itsrosemary79@gmail.com>
2022-09-14 12:58:35 +0100

    completed header banner section and about us section

---
+++ starter/main.js
@@ -0,0 +1,29 @@
+
+
+// alert("i have been coding since 12")
+
+// basic ES5 function declaration
+// function (){}
+
+var myname = "Dj RM"
+function popsomething() {
+    alert( myname + " i have been coding since 12")
+}
+
+// console.log shows things typed in the console
+// console.log(myname)
+// understanding the DOM
+// console.log(document)
+
+// document.querySelector(".ourpagebody").style.background = "blue"
+
+// var ourbody = document.querySelector(".ourpagebody")
+// ourbody.style.background = "black"
+
+// changing the harmburger
+var hamburger = document.querySelector(".hamburger-container")
+console.log(hamburger)
+
+function addandremove() {
+    hamburger.classList.toggle("showburger")
+}
